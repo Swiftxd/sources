af7cce079ea794ce61deede1914204bc44d2f3a1
Swiftxd <xande.oo@hotmail.com>
2016-03-21 13:09:42 -0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -2,10 +2,13 @@ bool Player::canSeeCreature(const Creature* creature) const
 {
 if(creature == this)
 return true;
+    const Player* player = creature->getPlayer();
+    const Player* playercid = this->getPlayer();
+    if (player->getParty() && playercid->getParty() && player->getParty() == playercid->getParty())
+    {
+        return true;
+    }
 if(const Player* player = creature->getPlayer())
 return !player->isGhost() || (getAccess() > 2 && getGhostAccess() >= player->getGhostAccess());
 return !creature->isInvisible() || canSeeInvisibility();
-	if(party)
-		party->isPlayerMember(const Player*) 
-			return !creature->isInvisible() || canSeeInvisibility();
 }
