c5843cc118ea99f90281e23e5a48e88863406a95
Swiftxd <xande.oo@hotmail.com>
2016-03-21 13:36:20 -0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,14 +1,56 @@
-bool Player::canSeeCreature(const Creature* creature) const
+bool TalkAction::ghost(Creature* creature, const std::string&, const std::string&)
 {
-if(creature == this)
-return true;
-    const Player* player = creature->getPlayer();
-    const Player* playercid = this->getPlayer();
-    if (player->getParty() && playercid->getParty() && player->getParty() == playercid->getParty())
-    {
-        return true;
-    }
-if(const Player* player = creature->getPlayer())
-return !player->isGhost() || (getAccess() > 2 && getGhostAccess() >= player->getGhostAccess());
-return !creature->isInvisible() || canSeeInvisibility();
+	Player* player = creature->getPlayer();
+	if(!player)
+		return false;
+
+	if(player->hasFlag(PlayerFlag_CannotBeSeen))
+	{
+		player->sendTextMessage(MSG_STATUS_CONSOLE_BLUE, "Command disabled for players with special, invisibility flag.");
+		return true;
+	}
+
+	SpectatorVec::iterator it;
+	SpectatorVec list = g_game.getSpectators(player->getPosition());
+	Player* tmpPlayer = NULL;
+
+	Condition* condition = NULL;
+	if((condition = player->getCondition(CONDITION_GAMEMASTER, CONDITIONID_DEFAULT, GAMEMASTER_INVISIBLE)))
+	{
+		player->sendTextMessage(MSG_INFO_DESCR, "You are visible again.");
+		for(AutoList<Player>::iterator pit = Player::autoList.begin(); pit != Player::autoList.end(); ++pit)
+		{
+
+		}
+
+		for(it = list.begin(); it != list.end(); ++it)
+		{
+			if((tmpPlayer = (*it)->getPlayer()) && !tmpPlayer->canSeeCreature(player))
+				tmpPlayer->sendMagicEffect(player->getPosition(), MAGIC_EFFECT_TELEPORT);
+		}
+
+		player->removeCondition(condition);
+		g_game.internalCreatureChangeVisible(creature, VISIBLE_GHOST_APPEAR);
+	}
+	else if((condition = Condition::createCondition(CONDITIONID_DEFAULT, CONDITION_GAMEMASTER, -1, 0, false, GAMEMASTER_INVISIBLE)))
+	{
+		player->addCondition(condition);
+		g_game.internalCreatureChangeVisible(creature, VISIBLE_GHOST_DISAPPEAR);
+		for(it = list.begin(); it != list.end(); ++it)
+		{
+			if((tmpPlayer = (*it)->getPlayer()) && !tmpPlayer->canSeeCreature(player))
+				tmpPlayer->sendMagicEffect(player->getPosition(), MAGIC_EFFECT_POFF);
+		}
+
+		for(AutoList<Player>::iterator pit = Player::autoList.begin(); pit != Player::autoList.end(); ++pit)
+		{
+		}
+
+		if(player->isTrading())
+			g_game.internalCloseTrade(player);
+
+		player->sendTextMessage(MSG_INFO_DESCR, "You are now invisible.");
+	}
+
+	return true;
 }
